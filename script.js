cf846e0ee2f5f0a79d326e5566168c0ed91538f4
Isabele Prieto Himeno <52426897+Prietoisa@users.noreply.github.com>
2019-07-11 10:22:14 -0300

    intro de javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+let NomeDoAluno = prompt ('nome do aluno');
+let Materia = prompt ('nome da materia');
+let Nota = prompt ('sua nota');
+
+let media = 6;
+
+if (Number(Nota)>= media) {
+    alert('Parabens, o aluno' + NomeDoAluno + 'foi aprovado na materia' + Materia);
+}
+
+else if (Number(Nota)>=media) {
+    alert ('')
+}
